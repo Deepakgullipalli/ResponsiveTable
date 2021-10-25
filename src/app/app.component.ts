1b6680890b71c67a11d5556afc6e202cd3f71af4
Deepak Gullipalli <deepakgullipalli@yahoo.com>
2021-10-26 00:05:42 +0530

    responsive UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './productservice';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'ResponsiveTable';
+export class AppComponent { 
+    
+
+    constructor(private productService: ProductService) { }
+    products: Product[] = [];
+
+    cols: any[] = [];
+    ngOnInit() {
+        this.productService.getProductsSmall().then(data => this.products = data);
+
+        this.cols = [
+            { field: 'code', header: 'Code' },
+            { field: 'name', header: 'Name' },
+            { field: 'category', header: 'Category' },
+            { field: 'quantity', header: 'Quantity' },
+            { field: 'inventoryStatus', header: 'Status' },
+            { field: 'rating', header: 'Rating' }
+        ];
+    }
 }
